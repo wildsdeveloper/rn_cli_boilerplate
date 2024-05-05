a064291f330c4bdb490fd228f61ba9ab354ed6b8
Wilds Rincon <wildsrincon.developer@gmail.com>
2024-05-05 10:28:29 -0500

    fix: remove react-test-renderer

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -2,16 +2,12 @@
  * @format
  */
 
-import 'react-native';
-import React from 'react';
+import {render, screen} from '@testing-library/react-native';
 import App from '../App';
 
-// Note: import explicitly to use the types shipped with jest.
-import {it} from '@jest/globals';
-
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-  renderer.create(<App />);
+describe('App', () => {
+  it('renders correctly', () => {
+    render(<App />);
+    expect(screen.getByText('Boilerplate RN CLI')).toBeTruthy();
+  });
 });
